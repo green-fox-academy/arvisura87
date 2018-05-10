3b3a5e1d35689f85093d69450cdc477a2709330e
arvisura87 <jakab.tamas@me.com>
2018-05-10 14:15:30 +0200

    07 done

---
+++ week-01/day-4/src/FavoriteNumber07.java
@@ -0,0 +1,8 @@
+public class FavoriteNumber07 {
+    public static void main(String[] args) {
+        int favorite_number = 12;
+        // Store your favorite number in a variable (as a number)
+        // And print it like this: "My favorite number is: 8"
+        System.out.println(favorite_number);
+    }
+}

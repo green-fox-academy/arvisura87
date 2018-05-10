82aeb2d94c059d16cb652f69ae64db66dbde7fc6
arvisura87 <jakab.tamas@me.com>
2018-05-10 14:22:47 +0200

    08 done

---
+++ week-01/day-4/src/Swap08.java
@@ -0,0 +1,18 @@
+public class Swap08 {
+    public static void main(String[] args) {
+        // Swap the values of the variables
+        int a = 123;
+        int b = 526;
+        int aa = a;
+        int bb = b;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        a = bb;
+        b = aa;
+        
+        System.out.println(a);
+        System.out.println(b);
+    }
+}

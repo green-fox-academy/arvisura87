d964cb0daf1380c3d2047a835d70c4e6f4b2c1ac
arvisura87 <jakab.tamas@me.com>
2018-05-10 13:31:45 +0200

    03 done

---
+++ week-01/day-4/src/HelloOthers03.java
@@ -0,0 +1,13 @@
+public class HelloOthers03 {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program, in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+        System.out.println("Hello Gábor!");
+        System.out.println("Hello Ica!");
+        System.out.println("Hello Ákos!");
+    }
+}

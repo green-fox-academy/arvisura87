aae4c8679219cc460fce9f9c48fd0994e4f5ad6f
arvisura87 <jakab.tamas@me.com>
2018-05-10 16:33:21 +0200

    10 done

---
+++ week-01/day-4/src/DefineBasicInfo10.java
@@ -0,0 +1,15 @@
+public class DefineBasicInfo10 {
+    public static void main(String[] args) {
+        // Define several things as a variable then print their values
+        // Your name as a string
+        // Your age as an integer
+        // Your height in meters as a double
+        // Whether you are married or not as a boolean
+        String name = "Tamas";
+        int age = 30;
+        double heigh = 1.86;
+        boolean married = false;
+
+        System.out.println(name + ", " + age + ", " + heigh + ", " + married);
+    }
+}

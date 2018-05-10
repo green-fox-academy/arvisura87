d48cb162799df80a0d3f420a5407ada952725ef0
arvisura87 <jakab.tamas@me.com>
2018-05-10 16:27:26 +0200

    09 done

--- week-01/day-4/src/Bmi09.java
+++ week-01/day-4/src/Bmi09.java
@@ -1,2 +1,14 @@
 public class Bmi09 {
+    public static void main(String[] args) {
+        double massInKg = 81.2;
+        double heightInM = 1.78;
+
+        // Print the Body mass index (BMI) based on these values
+        System.out.println(Bmi(massInKg, heightInM));
+    }
+
+    private static double Bmi(double massInKg, double heightInM) {
+        double bmi = massInKg/(heightInM*heightInM);
+        return bmi;
+    }
 }

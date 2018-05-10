f1bd54851a0b6046bd1e09f4f41a511f8c0830de
arvisura87 <jakab.tamas@me.com>
2018-05-10 14:08:20 +0200

    06 done

---
+++ week-01/day-4/src/CodingHours6.java
@@ -0,0 +1,17 @@
+public class CodingHours6 {
+    public static void main(String[] args) {
+        // An average Green Fox attendee codes 6 hours daily
+        // The semester is 17 weeks long
+        //
+        // Print how many hours is spent with coding in a semester by an attendee,
+        // if the attendee only codes on workdays.
+        //
+        // Print the percentage of the coding hours in the semester if the average
+        // work hours weekly is 52
+        System.out.println(5*6*17);
+        float f1 = 30;
+        float f2 = 52;
+        float f3 = f1/f2;
+        System.out.println(f3*100 + "%");
+    }
+}

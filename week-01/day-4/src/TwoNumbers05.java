20ba51b841b9595f9386488b49b30ca7c7f47887
arvisura87 <jakab.tamas@me.com>
2018-05-10 13:57:02 +0200

    05 done

---
+++ week-01/day-4/src/TwoNumbers05.java
@@ -0,0 +1,26 @@
+public class TwoNumbers05 {
+    public static void main(String[] args) {
+        // Create a program that prints a few operations on two numbers: 22 and 13
+
+        // Print the result of 13 added to 22
+
+        // Print the result of 13 substracted from 22
+
+        // Print the result of 22 multiplied by 13
+
+        // Print the result of 22 divided by 13 (as a decimal fraction)
+
+        // Print the integer part of 22 divided by 13
+
+        // Print the reminder of 22 divided by 13
+        int num1= 22;
+        int num2= 13;
+
+        System.out.println(num1+num2);
+        System.out.println(num2-num1);
+        System.out.println(num1*num2);
+        System.out.println((float)(num1)/ (float) (num2));
+        System.out.println(num1/num2);
+        System.out.println(num1%num2);
+    }
+}

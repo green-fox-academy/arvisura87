9cda5364cba00cf91384c79b327d2d55f63598ed
arvisura87 <jakab.tamas@me.com>
2018-05-14 16:56:17 +0200

    uniqe 1.0

--- week-02/day-1/src/Unique.java
+++ week-02/day-1/src/Unique.java
@@ -1,18 +1,33 @@
+import java.util.Arrays;
+
 public class Unique {
 
-/*
   public static void main(String[] args) {
     //  Create a function that takes a list of numbers as a parameter
     //  Returns a list of numbers where every number in the list occurs only once
 
     //  Example
-    System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
+    int[] listOfNumbers = {1, 11, 34, 11, 52, 61, 1, 34};
+    System.out.println(Arrays.toString(unique(listOfNumbers)));
     //  should print: `[1, 11, 34, 52, 61]`
 
 
   }
 
-*/
+  public static int[] unique(int[] ints) {
+    int[] uniqueNumbers = new int[ints.length];
+    for (int i = 0; i < ints.length; i++) {
+      for (int j = 0; j < uniqueNumbers.length; j++) {
+        if (ints[i]!=uniqueNumbers[j]){
+          uniqueNumbers[j]=ints[i];
+        } else {
+          break;
+        }
+      }
+    }
+    return uniqueNumbers;
+  }
+
 /*
   public static int[] unique(int[] list) {
 

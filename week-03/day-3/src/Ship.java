a9fd33f6fd942d6538f4c4c21af44f2f47e29565
arvisura87 <jakab.tamas@me.com>
2018-05-23 15:45:58 +0200

    feat(Ship): add fillShip function

--- week-03/day-3/src/Ship.java
+++ week-03/day-3/src/Ship.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ship {
+
+  ArrayList<Pirate> crew;
+
+  public void fillShip(Captain captain, Pirate... pirates){
+    this.crew = new ArrayList<>();
+    this.crew.add(captain);
+
+    for (int i = 1; i < pirates.length; i++) {
+      this.crew.add(pirates[i]);
+    }
+  }
 }

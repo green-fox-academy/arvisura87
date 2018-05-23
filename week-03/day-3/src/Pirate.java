3e2556322678c12e42b6768bd8692d484472f3a7
arvisura87 <jakab.tamas@me.com>
2018-05-23 14:28:09 +0200

    feat(Pirate): add brawl(function)

--- week-03/day-3/src/Pirate.java
+++ week-03/day-3/src/Pirate.java
@@ -1,11 +1,13 @@
 public class Pirate {
 
+  String name;
   boolean isAlive;
   boolean isAwake;
   boolean isToxic;
   int howManyDrinksWas;
 
-  public Pirate(){
+  public Pirate(String name){
+    this.name = name;
     this.isAlive = true;
     this.isAwake = true;
     this.isToxic = false;
@@ -36,5 +38,32 @@ public class Pirate {
 
   public void die(){
     this.isAlive = false;
+  }
+
+  public void brawl(Pirate enemy){
+    int endingChance = (int)(Math.random()*10);
+
+    if (this.isAlive && enemy.isAlive){
+      while (endingChance<1){
+        endingChance = (int)Math.random();
+      }
+      if (endingChance >= 1 && endingChance <= 3){
+        this.isAlive = false;
+        System.out.println("Battle over, " + this.name + "died!");
+      } else if (endingChance >= 4 && endingChance <= 6){
+        enemy.isAlive = false;
+        System.out.println("Battle over, " + enemy.name + "died!");
+      } else {
+        this.isAlive = false;
+        enemy.isAlive = false;
+        System.out.println("Battle over, " + this.name + "and" + enemy.name + " both died!");
+      }
+    } else if (this.isAlive){
+      System.out.println("Oh, " + enemy.name + " is already dead, they can not have a fight!");
+    } else if (enemy.isAlive){
+      System.out.println("Oh, " + this.name + " is already dead, they can not have a fight!");
+    } else {
+      System.out.println("Oh, they already dead, there will be no fight!");
+    }
   }
 }

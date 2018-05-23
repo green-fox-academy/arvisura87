c217e9bb8f809e72a0ed53c7d64baa87eee85ca5
arvisura87 <jakab.tamas@me.com>
2018-05-23 13:55:02 +0200

    feat and refactor(Pirate): add die() and refactor other functions to reflect for it

--- week-03/day-3/src/Pirate.java
+++ week-03/day-3/src/Pirate.java
@@ -1,20 +1,40 @@
 public class Pirate {
 
-  int drinkCounter = 0;
-  boolean isToxic = false;
-  boolean isAwake = true;
+  boolean isAlive;
+  boolean isAwake;
+  boolean isToxic;
+  int howManyDrinksWas;
+
+  public Pirate(){
+    this.isAlive = true;
+    this.isAwake = true;
+    this.isToxic = false;
+    this.howManyDrinksWas = 0;
+  }
 
   public void drinkSomeRum(){
-    this.isToxic = true;
-    this.drinkCounter++;
+    if (this.isAlive) {
+      this.isToxic = true;
+      this.howManyDrinksWas++;
+    } else {
+      System.out.println("he's dead");
+    }
   }
 
   public void howsItGoingMate(){
-    if (this.drinkCounter < 5){
-      System.out.println("Pour me anudder!");
+    if (this.isAlive){
+      if (this.howManyDrinksWas < 5){
+        System.out.println("Pour me anudder!");
+      } else {
+        System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+        this.isAwake = false;
+      }
     } else {
-      System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
-      this.isAwake = false;
+      System.out.println("he's dead");
     }
+  }
+
+  public void die(){
+    this.isAlive = false;
   }
 }

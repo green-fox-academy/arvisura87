d1d17a80a55d54aabdd5b035fba4d348c9a21c3e
arvisura87 <jakab.tamas@me.com>
2018-05-23 16:42:01 +0200

    feat(Ship): add toString() override

--- week-03/day-3/src/Ship.java
+++ week-03/day-3/src/Ship.java
@@ -12,4 +12,38 @@ public class Ship {
       this.crew.add(pirates[i]);
     }
   }
+
+  @Override
+  public String toString() {
+    String result = "";
+    String captainRum;
+    String captainState;
+    int alivePirates = 0;
+
+    if (crew.get(0).isToxic){
+      captainRum = "The captain did drink rum. ";
+    } else {
+      captainRum = "The captain did not drink rum. ";
+    }
+
+    if (crew.get(0).isAlive == true)
+      if (crew.get(0).isAwake == true){
+        captainState = "And he is awake. ";
+      } else {
+        captainState = "And he is passed out. ";
+      }
+      else {
+      captainState = "And he is dead. ";
+    }
+
+    for (int i = 1; i < crew.size(); i++) {
+      if (crew.get(i).isAlive){
+        alivePirates++;
+      }
+    }
+
+    result += captainRum + captainState + "And the number of alive pirates: " + alivePirates;
+
+    return result;
+  }
 }

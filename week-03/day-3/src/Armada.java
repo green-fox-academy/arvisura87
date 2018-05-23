6573b8d7b429664114f61751c24b8e6059be6b0f
arvisura87 <jakab.tamas@me.com>
2018-05-23 21:43:00 +0200

    feat(Armada): add fillArmada function for adding ships to the armada

--- week-03/day-3/src/Armada.java
+++ week-03/day-3/src/Armada.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Armada {
+
+  ArrayList<Ship> armada;
+
+  public void fillArmada(Ship... ships){
+    armada = new ArrayList<>();
+
+    for (int i = 0; i < ships.length; i++) {
+        armada.add(ships[i]);
+    }
+  }
 }

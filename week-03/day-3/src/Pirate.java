2583b8b176e168a6720c110ee869ce9abfee6f49
arvisura87 <jakab.tamas@me.com>
2018-05-23 13:14:24 +0200

    feat(pirate): functions create (drinkSomeRum, howsItGoingMate)

--- week-03/day-3/src/Pirate.java
+++ week-03/day-3/src/Pirate.java
@@ -1,2 +1,20 @@
 public class Pirate {
+
+  int drinkCounter = 0;
+  boolean isToxic = false;
+  boolean isAwake = true;
+
+  public void drinkSomeRum(){
+    this.isToxic = true;
+    this.drinkCounter++;
+  }
+
+  public void howsItGoingMate(){
+    if (this.drinkCounter < 5){
+      System.out.println("Pour me anudder!");
+    } else {
+      System.out.println("Arghh, I'ma Pirate. How d'ya d'ink its goin?");
+      this.isAwake = false;
+    }
+  }
 }

ca685c6d7f95959a704f7c21c8fdec7dbb206803
arvisura87 <jakab.tamas@me.com>
2018-05-24 14:05:17 +0200

    feat(Armada): created war function between armadas

--- week-03/day-3/src/Armada.java
+++ week-03/day-3/src/Armada.java
@@ -11,4 +11,23 @@ public class Armada {
         armada.add(ships[i]);
     }
   }
+
+  public boolean war(Armada enemy){
+    int thisShips = this.armada.size();
+    int enemyShips = enemy.armada.size();
+    int counterThisShip = 0;
+    int counterEnemyShip = 0;
+
+    while (thisShips != 0 && enemyShips != 0){
+      boolean whoWon = this.armada.get(counterThisShip).battle(enemy.armada.get(counterEnemyShip));
+        if (whoWon){
+          enemyShips--;
+          counterEnemyShip++;
+        } else {
+          thisShips--;
+          counterThisShip++;
+        }
+    }
+    return thisShips > enemyShips ? true : false;
+  }
 }

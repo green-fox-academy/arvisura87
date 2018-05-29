e3d2857fb9ea1444d7eec57ac3de156992c57200
arvisura87 <jakab.tamas@me.com>
2018-05-29 13:36:29 +0200

    feat(Garden): constructor for making list of plants

--- week-04/day-2/garden/src/Garden.java
+++ week-04/day-2/garden/src/Garden.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Garden {
+
+  private List<Plant> plantList;
+
+  public Garden(Plant... plants) {
+    plantList = new ArrayList<>();
+
+    for (Plant plant: plants) {
+      plantList.add(plant);
+    }
+  }
 }

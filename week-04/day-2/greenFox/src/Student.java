a2cf4f9bd2274b034f40f708c0947fe6f2dc26c7
arvisura87 <jakab.tamas@me.com>
2018-05-29 15:45:25 +0200

    feat(Student): constructors created

--- week-04/day-2/greenFox/src/Student.java
+++ week-04/day-2/greenFox/src/Student.java
@@ -1,2 +1,11 @@
 public class Student extends Person {
+
+  private String previousOrganization;
+  private int skippedDays;
+
+  public Student(String name, int age, String gender, String previousOrganization) {
+    super();
+    this.previousOrganization = previousOrganization;
+    this.skippedDays = 0;
+  }
 }

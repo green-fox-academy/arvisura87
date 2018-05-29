4f4f55c8bd1c363d501f4fa83f2a72f824faaa4a
arvisura87 <jakab.tamas@me.com>
2018-05-29 16:05:15 +0200

    feat(Mentor): constructors created

--- week-04/day-2/greenFox/src/Mentor.java
+++ week-04/day-2/greenFox/src/Mentor.java
@@ -1,4 +1,13 @@
 public class Mentor extends Person {
 
   private String level;
+
+  public Mentor() {
+    this.level = "intermediate";
+  }
+
+  public Mentor(String name, int age, String gender, String level) {
+    super(name, age, gender);
+    this.level = level;
+  }
 }

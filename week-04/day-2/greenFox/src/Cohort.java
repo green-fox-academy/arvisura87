fd081120b9b8bad1eda7a7cc48635361e2351fbb
arvisura87 <jakab.tamas@me.com>
2018-05-29 16:51:01 +0200

    feat(Cohort): added functions to add students and mentors to the lists

--- week-04/day-2/greenFox/src/Cohort.java
+++ week-04/day-2/greenFox/src/Cohort.java
@@ -1,8 +1,23 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cohort {
 
   private String name;
-  private List<Student> students;
-  private List<Mentor> mentors;
+  private List<Student> studentList;
+  private List<Mentor> mentorList;
+
+  public Cohort(String name) {
+    this.name = name;
+    this.students = new ArrayList<Student>();
+    this.mentors = new ArrayList<Mentor>();
+  }
+
+  public void addStudent(Student student) {
+    studentList.add(student);
+  }
+
+  public void addMentor(Mentor mentor) {
+    mentorList.add(mentor);
+  }
 }

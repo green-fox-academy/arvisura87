ee8fecb606b8692d98e7154ed0ba31d362b0e482
arvisura87 <jakab.tamas@me.com>
2018-05-29 13:19:35 +0200

    feat(Plant): created constructor for asking color and a watering function

--- week-04/day-2/garden/src/Plant.java
+++ week-04/day-2/garden/src/Plant.java
@@ -1,2 +1,14 @@
 public class Plant {
+
+  private String color;
+  private double currentWater;
+
+  public Plant(String color) {
+    this.color = color;
+    this.currentWater = 0.0;
+  }
+
+  public void watering(double amountOfWater) {
+    this.currentWater += amountOfWater;
+  }
 }

bd535f9cb6b7d3aaf7fc13603d24e71e481dce1b
arvisura87 <jakab.tamas@me.com>
2018-05-31 14:00:44 +0200

    feat(Sum&Test): work with a empty list

--- week-04/day-4/testExercises/test/SumTest.java
+++ week-04/day-4/testExercises/test/SumTest.java
@@ -16,10 +16,19 @@ public class SumTest {
   }
 
   @Test
-  public void sumOfIntList() {
+  public void should_return6_with_input1_2_3() {
     ArrayList<Integer> integerList = new ArrayList<>(Arrays.asList(1, 2, 3));
     int result = sum.sumOfIntList(integerList);
     int expected = 6;
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void should_return0_with_inputEmptyList() {
+    ArrayList<Integer> integerList = new ArrayList<>();
+    int result = sum.sumOfIntList(integerList);
+    int expected = 0;
 
     assertEquals(expected, result);
   }

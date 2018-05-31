13377c4c818124fc5bff4c43d52779684bcaaab6
arvisura87 <jakab.tamas@me.com>
2018-05-31 14:07:18 +0200

    feat(Sum&Test): work with one element in the list

--- week-04/day-4/testExercises/test/SumTest.java
+++ week-04/day-4/testExercises/test/SumTest.java
@@ -1,4 +1,5 @@
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,15 +10,22 @@ import static org.junit.Assert.*;
 public class SumTest {
 
   static Sum sum;
+  static ArrayList<Integer> integerList;
+
 
   @Before
   public void init() {
     sum = new Sum();
   }
 
+  @BeforeClass
+  public static void initClass() {
+    integerList = new ArrayList<>();
+  }
+
   @Test
   public void should_return6_with_input1_2_3() {
-    ArrayList<Integer> integerList = new ArrayList<>(Arrays.asList(1, 2, 3));
+    integerList = new ArrayList<>(Arrays.asList(1, 2, 3));
     int result = sum.sumOfIntList(integerList);
     int expected = 6;
 
@@ -26,9 +34,17 @@ public class SumTest {
 
   @Test
   public void should_return0_with_inputEmptyList() {
-    ArrayList<Integer> integerList = new ArrayList<>();
     int result = sum.sumOfIntList(integerList);
     int expected = 0;
+
+    assertEquals(expected, result);
+  }
+
+  @Test
+  public void should_returnTheOneIntIn_with_inputOneInt() {
+    integerList = new ArrayList<>(Arrays.asList(3));
+    int result = sum.sumOfIntList(integerList);
+    int expected = 3;
 
     assertEquals(expected, result);
   }

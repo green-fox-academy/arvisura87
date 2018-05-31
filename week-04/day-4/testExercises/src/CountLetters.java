900f53bbefc1cec311f23b3bf56162bbb5498d8d
arvisura87 <jakab.tamas@me.com>
2018-05-31 17:02:23 +0200

    init&feat(CountLetters): counting letters occurence in string

---
+++ week-04/day-4/testExercises/src/CountLetters.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+public class CountLetters {
+
+  public HashMap<Character, Integer> countLettersOccurrenceInString(String word1) {
+    HashMap<Character, Integer> map = new HashMap<>();
+    String word = word1.toLowerCase();
+
+    for (int i = 0; i < word.length(); i++) {
+      int counter = 0;
+
+      for (int j = 0; j < word.length(); j++) {
+
+        if (word.charAt(i) == word.charAt(j)) {
+          counter++;
+        }
+        map.put(word.charAt(i), counter);
+      }
+    }
+    return map;
+  }
+}

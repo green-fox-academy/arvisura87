57b99eb043374d5940305b2487a7977154593214
arvisura87 <jakab.tamas@me.com>
2018-05-31 13:57:15 +0200

    feat(Sum&Test): work with a integer list

--- week-04/day-4/testExercises/src/Sum.java
+++ week-04/day-4/testExercises/src/Sum.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Sum {
+
+    public int sumOfIntList(ArrayList<Integer> integerArrayList) {
+      int result = 0;
+
+      for (Integer integer : integerArrayList) {
+        result += integer;
+      }
+      return result;
+    }
 }
--- week-04/day-4/testExercises/test/SumTest.java
+++ week-04/day-4/testExercises/test/SumTest.java
@@ -1,5 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class SumTest {
 
+  static Sum sum;
+
+  @Before
+  public void init() {
+    sum = new Sum();
+  }
+
+  @Test
+  public void sumOfIntList() {
+    ArrayList<Integer> integerList = new ArrayList<>(Arrays.asList(1, 2, 3));
+    int result = sum.sumOfIntList(integerList);
+    int expected = 6;
+
+    assertEquals(expected, result);
+  }
 }

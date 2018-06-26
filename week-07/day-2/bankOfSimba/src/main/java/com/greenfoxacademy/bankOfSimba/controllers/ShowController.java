dbf8b5094a0e4d4b6d2ff12c58a47fc98c6d1a25
arvisura87 <jakab.tamas@me.com>
2018-06-26 15:30:58 +0200

    feat(bankOfSimba): ad /listaccounts endpoint for listing the accounts in a table

--- week-07/day-2/bankOfSimba/src/main/java/com/greenfoxacademy/bankOfSimba/controllers/ShowController.java
+++ week-07/day-2/bankOfSimba/src/main/java/com/greenfoxacademy/bankOfSimba/controllers/ShowController.java
@@ -5,10 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class ShowController {
 
   private BankAccount bankAccount;
+  private List<BankAccount> bankAccountList;
 
   @GetMapping("/show")
   public String showShowPage(Model model) {
@@ -24,5 +29,20 @@ public class ShowController {
     model.addAttribute("captionText", captionText);
 
     return "htmlcaption";
+  }
+
+  @GetMapping("/listaccounts")
+  public String showListOfAccounts(Model model) {
+    BankAccount zordon = new BankAccount("Zordon", 3000, "lion");
+    BankAccount nala = new BankAccount("Nala", 500, "lion");
+    BankAccount muszafa = new BankAccount("Muszafa", 5000, "lion");
+    BankAccount rafiki = new BankAccount("Rafiki", 300, "monkey");
+    BankAccount zazu = new BankAccount("Zazu", 100, "parrot");
+
+    bankAccountList = new ArrayList<>();
+    bankAccountList = Arrays.asList(zordon, nala, muszafa, rafiki, zazu);
+    model.addAttribute("bankAccountList", bankAccountList);
+
+    return "listaccounts";
   }
 }

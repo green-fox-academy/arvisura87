8f706634b3fd7da414a09d9eaf1a4d907cad5a69
arvisura87 <jakab.tamas@me.com>
2018-06-27 14:02:48 +0200

    feat(HelloBeanWorld): ad commandline print function

--- week-07/day-3/src/main/java/com/greenfoxacademy/hellobeanworld/HellobeanworldApplication.java
+++ week-07/day-3/src/main/java/com/greenfoxacademy/hellobeanworld/HellobeanworldApplication.java
@@ -1,12 +1,23 @@
 package com.greenfoxacademy.hellobeanworld;
 
+import com.greenfoxacademy.hellobeanworld.services.PrinterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HellobeanworldApplication {
+public class HellobeanworldApplication implements CommandLineRunner {
+
+  @Autowired
+  PrinterService service;
 
   public static void main(String[] args) {
     SpringApplication.run(HellobeanworldApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    service.log("Tamás");
   }
 }

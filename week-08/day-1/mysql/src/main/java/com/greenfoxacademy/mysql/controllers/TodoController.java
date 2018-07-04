a51c0f2a9588b38616ea86a51d3a95da9a9e88b8
arvisura87 <jakab.tamas@me.com>
2018-07-04 17:16:45 +0200

    feat(mysql): ad adding new todo feature and page

--- week-08/day-1/mysql/src/main/java/com/greenfoxacademy/mysql/controllers/TodoController.java
+++ week-08/day-1/mysql/src/main/java/com/greenfoxacademy/mysql/controllers/TodoController.java
@@ -1,12 +1,11 @@
 package com.greenfoxacademy.mysql.controllers;
 
+import com.greenfoxacademy.mysql.models.Todo;
 import com.greenfoxacademy.mysql.repositories.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/todo")
@@ -16,12 +15,24 @@ public class TodoController {
   private TodoRepository todoRepository;
 
   @GetMapping(value={"/", "/list"})
-  public String list(Model model, @RequestParam(value = "isDone", required = false) boolean isDone) {
-    if (isDone) {
-      model.addAttribute("todoList", todoRepository.findByDone(true));
+  public String list(Model model, @RequestParam(value = "isDone", required = false) Boolean isDone) {
+    if (isDone == null) {
+      model.addAttribute("todoList", todoRepository.findAll());
     } else {
-      model.addAttribute("todoList", todoRepository.findByDone(false));
+      model.addAttribute("todoList", todoRepository.findByDone(isDone));
     }
     return "todolist";
+  }
+
+  @GetMapping("/add")
+  public String add(Model model) {
+    model.addAttribute("todo", new Todo());
+    return "add";
+  }
+
+  @PostMapping("/add")
+  public String sumbit(@ModelAttribute Todo todo) {
+    todoRepository.save(todo);
+    return "redirect:/todo/";
   }
 }

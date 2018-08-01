357394cc92830819211f4fe7935e912ce37d1788
arvisura87 <jakab.tamas@me.com>
2018-08-01 15:09:17 +0200

    init(mybrilliantthoughts): init project with db connection

---
+++ projectphase/src/main/java/com/greenfoxacademy/my_brilliant_thoughts/MyBrilliantThoughtsApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.my_brilliant_thoughts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBrilliantThoughtsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyBrilliantThoughtsApplication.class, args);
+  }
+}
